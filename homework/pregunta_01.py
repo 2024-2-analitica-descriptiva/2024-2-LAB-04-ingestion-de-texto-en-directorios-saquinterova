568f3c64053499f570f2e95fde9c7968b4d3e802
SQV <saquinterova@unal.edu.co>
2025-01-26 13:58:28 +0000

    try

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -71,3 +71,47 @@ def pregunta_01():
 
 
     """
+
+    import os
+    import zipfile
+
+    import pandas as pd
+
+
+    def create_directory(directory_name):
+        # Define the path to the files folder
+        files_folder = "files"
+        full_path = os.path.join(files_folder, directory_name)
+        
+        try:
+            os.makedirs(full_path)
+            print(f"Directory '{full_path}' created successfully!")
+        except FileExistsError:
+            print(f"Directory '{full_path}' already exists.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    create_directory("input")
+    create_directory("output")
+
+
+    # Descomprimir archivo
+    with zipfile.ZipFile("files/input.zip", "r") as zip_ref:
+        zip_ref.extractall("files/input")
+
+    # Leer archivos
+    data = []
+    for root, _, files in os.walk("files/input"):
+        for file in files:
+            with open(os.path.join(root, file), "r") as f:
+                data.append({"phrase": f.read(), "target": os.path.basename(root)})
+
+    # Crear dataframe
+    df = pd.DataFrame(data)
+
+    # Crear archivos
+    os.makedirs("output", exist_ok=True)
+    df[df["target"].str.contains("train")].to_csv("files/output/train_dataset.csv", index=False)
+    df[df["target"].str.contains("test")].to_csv("files/output/test_dataset.csv", index=False)
+
+pregunta_01()
